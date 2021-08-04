e7d084558d22bfe53114d0b3e544bb046131ee42
minhle251 <letrongminh2505@gmail.com>
2021-08-04 23:55:24 +0700

    update scroll function

--- CafeF_Data_Crawl.py
+++ CafeF_Data_Crawl.py
@@ -1,43 +1,33 @@
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.common.by import By
 
 # chrome_options = Options()
 # chrome_options.add_argument("--incognito")
 # chrome_options.add_argument("--window-size=1920*1080")
 
-driver = webdriver.Chrome(executable_path = "D:\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(executable_path = "chromedriver.exe")
+driver.maximize_window()
 driver.get('https://cafef.vn/thi-truong-chung-khoan.chn')
 sleep(3)
 count_page = 0
 
-# SCROLL_PAUSE_TIME = 0.5
-
-# # Get scroll height
-# last_height = driver.execute_script("return document.body.scrollHeight")
-
-# while True:
-#     # Scroll down to bottom
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-#     # Wait to load page
-#     sleep(SCROLL_PAUSE_TIME)
-
-#     # Calculate new scroll height and compare with last scroll height
-#     new_height = driver.execute_script("return document.body.scrollHeight")
-#     if new_height == last_height:
-#         break
-#     last_height = new_height
-
-def Crawl_article():
-    global count_page
-    count_page += 1
-    # links = driver.find_element_by_xpath('//*[@id="LoadListNewsCat"]/li[1]/h3/a')
-    links = driver.find_elements_by_tag_name('a')
+while True:
+    for i in range(5):
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+        sleep(2)
+    
+    links = driver.find_elements_by_xpath('//li[@class="tlitem clearfix ck_success"]/h3/a')
+    
     urls = []
     count = 0
-    #get urls
+    
     for link in links:
-        url = link.get_attribute("href")
+        print(link.text)
+        url = link.get_attribute('href')
+        # print(url)
         urls.append(url)
-        print(urls, "This is the urls we are looking for")
-Crawl_article()
+    
+    xemthem = driver.find_element_by_class_name('bt_xemthem')
+    xemthem.click()
+
